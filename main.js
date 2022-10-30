b9942ad6e8a54e659ee2e2bef2be84ae0c3b1c14
Rafael BC Dev <rafaelbc.dev@gmail.com>
2022-10-30 16:55:04 +0100

    Initial commit. Website ready for deployment.

---
+++ main.js
@@ -0,0 +1,36 @@
+// Menu button animation
+
+const menuBtn = document.getElementById('menu-btn')
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('change');
+
+    if (menuBtn.classList.contains('change')) {
+        document.getElementById('menu').style.width = '12em'
+        document.getElementById('menu-btn').style.right = '16em'
+        return
+    }
+
+    document.getElementById('menu').style.width = '0'
+    document.getElementById('menu-btn').style.right = '4em'
+})
+
+
+// Theme switch
+const switchBtn = document.getElementById('switch-btn')
+const switchToggle = document.getElementById('switch-toggle')
+
+switchBtn.addEventListener('click', () => {
+    
+    document.body.classList.toggle('dark')
+    switchBtn.classList.toggle('active')
+
+    if (switchBtn.classList.contains('active')) {
+        switchToggle.style.left = 'unset'
+        switchToggle.style.right = '0'
+        return
+    }
+    
+    switchToggle.style.left = '0'
+    switchToggle.style.right = 'unset'
+})
